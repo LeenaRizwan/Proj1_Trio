7b6bf6c5f35e23819d8d965f23d762867484b233
LeenaRizwan <i210430@nu.edu.pk>
2024-07-19 15:32:10 +0500

    Barebones login/signup

---
+++ InternshipProj1/app/src/main/java/com/example/internshipproj1/Login.kt
@@ -0,0 +1,36 @@
+package com.example.internshipproj1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        var login=findViewById<Button>(R.id.log)
+        var email=findViewById<EditText>(R.id.Email)
+        var pass=findViewById<EditText>(R.id.Password)
+        var mAuth=FirebaseAuth.getInstance()
+        login.setOnClickListener{
+                mAuth.signInWithEmailAndPassword(
+                    email.text.toString(),
+                    pass.text.toString()
+                ).addOnSuccessListener {
+                    Toast.makeText(this,"Succeeded To SignIn", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                    .addOnFailureListener {
+
+                        Log.e("Signin_Error",it.message.toString())
+                        Toast.makeText(this,"Failed To SignIn", Toast.LENGTH_LONG).show()
+                    }
+        }
+
+    }
+}
